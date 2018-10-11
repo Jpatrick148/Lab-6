7f4261c26c6a63c8a90d12f4f9e91a0ecf06bd8e
Joseph <Jpatrick148@Gmail.com>
2018-10-11 15:49:46 -0400

    Without DiceApp CLASS

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Please enter the amount of sides on the pair of dice");
+                int side = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ready to roll? Hit enter!");
+                Console.ReadLine();
+
+                Console.WriteLine(Dice(side));
+                Console.WriteLine(Dice(side));
+            }
+            while (Continue()==true);
+
+        }
+        public static readonly Random rnd = new Random();
+        public static int Dice(int i)
+        {
+            lock (rnd)
+            {
+                return rnd.Next(1,i+1);
+            }              
+        }
+
+        public static bool Continue()
+        {
+         Console.WriteLine("Again? {Y/N}");
+            string input = Console.ReadLine().ToLower();
+            if (input == "y")
+            {
+                return true;
+            }
+            else if (input == "n")
+            {
+                return false;
+            }
+            else Console.WriteLine("Please Try Again.");
+            return Continue();
+        }
+    }
+
+    class RollMsg
+    {
+        
+    }
+}
